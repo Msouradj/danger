ba2d585decc74c0884f7dbcca0fd45027d49a04f
souradj <souradj.mounien-dit-ravi@etu.u-paris.fr>
2023-01-13 16:27:55 +0100

    Maj des fichiers .c .h

---
+++ voiture.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include "voiture.h"
+
+int main(){
+	printf("Hello");
+}
---
+++ voiture.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include "voiture.h"
+
+
+
+typedef struct voiture{
+float vitesse; // en m/s
+} voiture;
