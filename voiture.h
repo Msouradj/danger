d502a0e1ade54117d71e72ffac34eb500193efa8
souradj <souradj.mounien-dit-ravi@etu.u-paris.fr>
2023-01-13 16:38:44 +0100

    Maj du fichier .c .h

--- voiture.h
+++ voiture.h
@@ -1,12 +1,14 @@
+#ifndef VOITURE_H
+#define VOITURE_H
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
-#include "voiture.h"
-
 
 
 typedef struct voiture{
 float vitesse; // en m/s
 } voiture;
+
+#endif
